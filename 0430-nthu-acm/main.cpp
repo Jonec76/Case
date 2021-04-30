4987657713aae2f6c468e541aa0d1c87971d1938
jonec76 <jonec9676@gmail.com>
2021-04-30 17:30:24 +0800

    Fix 0430-nthu-acm bug

--- 0430-nthu-acm/main.cpp
+++ 0430-nthu-acm/main.cpp
@@ -1,11 +1,22 @@
-// C++ program for the above approach
 #include <string>
 #include <iostream>
 #include <queue>
 #include <vector>
 using namespace std;
+
+// nthu-acm problem 13190
+// 問題分析
+
+/**
+ * 看到題目說明，是要用 linked-list 建立一個 tree 出來，並且計算到 leaf 的最短路徑。
+ * 其實這個有點像要誤導ＸＤ並不用使用到 linked-list，這個其實是用 BFS 跑出來的一個 tree
+ * 所以其實只要跑 BFS 即可，下方主要是依據 BFS 的演算法寫出來的解。
+ */
+
+
 int ROW, COL;
 
+// 定義上下左右的 action
 int dRow[] = { -1, 0, 1, 0 };
 int dCol[] = { 0, 1, 0, -1 };
 
@@ -29,6 +40,11 @@ void BFS(int** grid, bool** vis,
     pair<int, int>first_node;
     first_node.first = row;
     first_node.second = col;
+
+    if(grid[row][col] == -1){
+        cout<<0<<"\n";
+        return;
+    }    
 	q.push(first_node);
 	vis[row][col] = true;
 
@@ -48,15 +64,24 @@ void BFS(int** grid, bool** vis,
 			int adjy = y + dCol[i];
 
 			if (isValid(vis, adjx, adjy)) {
+                // 如果我還有鄰居，表示我不是 leaf
                 leaf = false;
-                pair<int, int>adj_node;
-                adj_node.first = adjx;
-                adj_node.second = adjy;
-				q.push(adj_node);
-                grid[adjx][adjy] += grid[x][y];
-				vis[adjx][adjy] = true;
+
+                // 如果這個鄰居不是死路，才可以把該鄰居加入 stack，否則就不考慮他
+                if(grid[adjx][adjy] != -1){
+                    pair<int, int>adj_node;
+                    adj_node.first = adjx;
+                    adj_node.second = adjy;
+                    q.push(adj_node);
+
+                    // 擴散 BFS，會把目前的值往鄰居加上去，如此一來就可以知道 root 到每個點的距離了
+                    grid[adjx][adjy] += grid[x][y];
+                }
+                vis[adjx][adjy] = true;
 			}
 		}
+
+        // 把所有 leaf 加入一個 vector
         if(leaf){
             pair<int, int>n;
             n.first = x;
@@ -64,8 +89,9 @@ void BFS(int** grid, bool** vis,
             leaf_node.push_back(n);  
         }   
 	}
-
-    int min = INT16_MAX;
+    // 這邊錯了幾次，因為 xy 最大為 10^5 ，加上距離 range 最高可能到 10^6，所以要有一個比 10^6 還要大的數字來找 min
+    // 這邊是把所有 leaf 列出來，並且找最小的 leaf。
+    int min = INT32_MAX;
     for(int i=0;i<leaf_node.size();i++){
         int leaf_x = leaf_node[i].first;
         int leaf_y = leaf_node[i].second;
@@ -74,13 +100,14 @@ void BFS(int** grid, bool** vis,
         }
     }
     cout<< min<<"\n";
-
 }
 
 int main()
 {
     int start_x, start_y;
     cin >> ROW >> COL;
+
+    // 他的測資的第一個值其實是 y position，所以要先 input start_y 再 input start_x
     cin >> start_y >>start_x;
 
     int *grid[ROW];
@@ -106,6 +133,9 @@ int main()
 
         }
     }
-    BFS(grid, vis, start_x, start_y);
+    if(vis[start_x][start_y] == true)
+        cout<<0<<"\n";
+    else
+        BFS(grid, vis, start_x, start_y);
 	return 0;
 }
