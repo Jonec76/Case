3b6707466ce73312e616ab4df36998f6fa7a0e7e
jonec76 <jonec9676@gmail.com>
2021-06-18 11:45:47 +0800

    0627 finish

--- 0627-ds/q1/main.cpp
+++ 0627-ds/q1/main.cpp
@@ -1,7 +1,12 @@
 #include<iostream>
+#include <random>
+#include <string>
+#include <iomanip>
 #include "insert_sort.h"
 #include "mid_of_three.h"
 #include "r_merge_sort.h"
+#include "i_merge_sort.h"
+#include "heap_sort.h"
 using namespace std;
 
 
@@ -28,29 +33,80 @@ void gen_float_data(T** arr, int len){
     }
 }
 
+void gen_random(char *s, size_t len) {
+     for (size_t i = 0; i < len; ++i) {
+        int randomChar = rand()%(26);
+        s[i] = 'a' + randomChar;
+     }
+     s[len] = 0;
+ }
+
+template < class T>
+void gen_string_data(T** arr, int len){
+
+    for(int i=1;i<=len;i++){
+        char* tmp = new char[5];
+        gen_random(tmp, 5);
+        string str(tmp);
+        (*arr)[i] = tmp;
+    }
+}
+
+template < class T>
+void print_arr(T* arr, int len){
+    for(int i=1;i<len;i++)
+        cout<<arr[i]<<" ";
+    puts("");
+}
+
 int main(){
-    int len=5;
+    int len=6;
     int* link = new int[len+1];
     int idx=0;
-    memset(link, 0, len*sizeof(int));
+    memset(link, 0, (len+1)*sizeof(int));
 
     srand(time(0));
     
-    int* i_arr = new int[len+1];
+
+    // generate random data
+    int* i_arr = new int[len];
     gen_int_data(&i_arr, len);
 
-    char* c_arr = new char[len+1];
+    char* c_arr = new char[len];
     gen_char_data(&c_arr, len);
 
-    float* f_arr = new float[len+1];
+    float* f_arr = new float[len];
     gen_float_data(&f_arr, len);
 
-    // InsertionSort(i_arr, len);
-    // mid_of_three(i_arr, 1, len);
-    // rMergeSort(c_arr, link, 1, len);
-    // while(link[idx]!=0){// link 裡面記錄的是 sorted list 的 next node index，從 link[0] 開始 
-    //     cout<<c_arr[link[idx]]<<" ";
-    //     idx = link[idx];
-    // }
+    string* s_arr = new string[len];
+    gen_string_data(&s_arr, len);
 
+
+
+    // sort 
+    cout<<setw(22)<<"Insertion sort: ";
+    InsertionSort(i_arr, len);
+    print_arr(i_arr, len);
+
+    cout<<setw(22)<<"Heap sort: ";
+    int* i_arr_2 = new int[len];
+    gen_int_data(&i_arr_2, len);
+    HeapSort(i_arr_2, len);
+    print_arr(i_arr_2, len);
+
+    cout<<"Midian-of-three sort: ";
+    mid_of_three(c_arr, 1, len);
+    print_arr(c_arr, len);
+
+    cout<<"Recursive merge sort: ";
+    rMergeSort(f_arr, link, 1, len);
+    while(link[idx]!=0){// link 裡面記錄的是 sorted list 的 next node index，從 link[0] 開始 
+        cout<<f_arr[link[idx]]<<" ";
+        idx = link[idx];
+    }
+    puts("");
+
+    cout<<"Iterative merge sort: ";
+    iMergeSort(s_arr, len);
+    print_arr(s_arr, len);
 }
